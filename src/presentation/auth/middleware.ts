510fa9297b0d0f628be9064c6040434b06b1e383
Lautaro Farias <farias.lautaro002@gmail.com>
2024-05-27 12:04:26 -0300

    feat: Add Authorization header extract

--- src/presentation/auth/middleware.ts
+++ src/presentation/auth/middleware.ts
@@ -2,6 +2,24 @@ import type { NextFunction, Request, Response } from "express"
 
 export class AuthMiddleware {
   static validateJWT = (req: Request, res: Response, next: NextFunction) => {
-    next()
+    const auth = req.header("Authorization")
+
+    if (!auth || !auth.startsWith("Bearer ")) return res.status(401).end()
+
+    const token = auth.split(" ").at(1) ?? ""
+
+    try {
+      // const payload = JWT
+
+      req.body.token = token
+
+      console.log(req.body) // token should be in body
+
+      next()
+    } catch (error) {
+      console.error(error)
+
+      return res.status(500).end()
+    }
   }
 }
