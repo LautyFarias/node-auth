ffa32c1a512a7a59c161949d28dc61843659fc76
Lautaro Farias <farias.lautaro002@gmail.com>
2024-05-27 16:55:02 -0300

    refactor: Format & separate types in register.dto

--- src/domain/dtos/auth/register.dto.ts
+++ src/domain/dtos/auth/register.dto.ts
@@ -1,15 +1,16 @@
+import { Result } from "../../../types"
 import { RegEx } from "../../../utils"
-
-type Result<A, B> = [A, undefined] | [undefined, B]
 
 export class RegisterUserDTO {
   private constructor(
     public name: string,
     public email: string,
-    public password: string
-  ) { }
+    public password: string,
+  ) {}
 
-  static create(object: { [key: string]: any }): Result<RegisterUserDTO, String> {
+  static create(object: {
+    [key: string]: any
+  }): Result<RegisterUserDTO, String> {
     const { name, email, password } = object
 
     if (!name) return [undefined, "Missing name"]
@@ -19,8 +20,12 @@ export class RegisterUserDTO {
     if (!RegEx.email.test(email)) return [undefined, "Invalid email"]
 
     if (!password) return [undefined, "Missing password"]
-    if (typeof password !== "string" || password.length < 6) [undefined, "Invalid password"]
+    if (typeof password !== "string" || password.length < 6)
+      [undefined, "Invalid password"]
 
-    return [new RegisterUserDTO(name as string, email as string, String(password)), undefined]
+    return [
+      new RegisterUserDTO(name as string, email as string, String(password)),
+      undefined,
+    ]
   }
 }
