4501921143a146333ce650c38ebbfd69d4e465c0
Lautaro Farias <farias.lautaro002@gmail.com>
2024-05-27 17:36:05 -0300

    test: describe test case for validation errors

--- __tests__/login.spec.ts
+++ __tests__/login.spec.ts
@@ -2,8 +2,8 @@ import { describe, expect, test } from "vitest"
 
 describe("Login", () => {
   describe("User logs in successfully", () => {
-    test("should give a response with a status code 200 an a body with the user and its token", async () => {
-      const response = await fetch("http://localhost:3000", {
+    test("should give a response with a status code 200 and a body with the user and its token", async () => {
+      const response = await fetch("http://localhost:3000/auth/login", {
         method: "POST",
         body: JSON.stringify({
           email: "john.doe@example.com",
@@ -18,6 +18,22 @@ describe("Login", () => {
       expect(data).to.contains("token", "user")
       expect(data.user).to.contains(["email", "id", "name"])
       expect(data.user.email).to.be("john.doe@example.com")
+    })
+  })
+  describe("User logs in unsuccessfully", () => {
+    describe("Client doesn't send required data in body", () => {
+      test("should give a response with a status code 400 and a body with the error", async () => {
+        const response = await fetch("http://localhost:3000/auth/login", {
+          method: "POST",
+        })
+
+        expect(response.status).to.be.equal(400)
+
+        const data = await response.json()
+
+        expect(data).to.have.property("error")
+        expect(data.error).to.be.equal("Missing email")
+      })
     })
   })
 })
