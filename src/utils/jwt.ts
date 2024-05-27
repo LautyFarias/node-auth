e1a8abc25cc7e6ef2e8f33addc32dcf8c9d0794f
Lautaro Farias <farias.lautaro002@gmail.com>
2024-05-27 12:14:11 -0300

    style: format jwt util

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -5,10 +5,13 @@ interface Options {
 }
 
 export class JWT {
-  static async generate(payload: Object, options: Options = { expiration: "2h" }) {
+  static async generate(
+    payload: Object,
+    options: Options = { expiration: "2h" },
+  ) {
     const { expiration: expiresIn } = options
 
-    return new Promise<string | null>(resolve => {
+    return new Promise<string | null>((resolve) => {
       jwt.sign(payload, "SEED", { expiresIn }, (err, token) => {
         if (err) return resolve(null)
 
