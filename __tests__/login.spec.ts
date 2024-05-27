59a06e39840e9a49a6aac9ce3897d1af930ad897
Lautaro Farias <farias.lautaro002@gmail.com>
2024-05-27 18:17:42 -0300

    test: fix test assertions

--- __tests__/login.spec.ts
+++ __tests__/login.spec.ts
@@ -5,7 +5,7 @@ describe("Login", () => {
     test("should give a response with a status code 200 and a body with the user and its token", async () => {
       const response = await fetch("http://localhost:3000/auth/login", {
         method: "POST",
-headers: {
+        headers: {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
@@ -18,9 +18,9 @@ headers: {
 
       const data = await response.json()
 
-      expect(data).to.contains("token", "user")
-      expect(data.user).to.contains(["email", "id", "name"])
-      expect(data.user.email).to.be("john.doe@example.com")
+      expect(data).to.have.keys("token", "user")
+      expect(data.user).to.have.keys("id", "name", "email")
+      expect(data.user.email).to.be.equal("john.doe@example.com")
     })
   })
   describe("User logs in unsuccessfully", () => {
