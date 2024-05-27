03e052ab5275cf8b43bb95f8eee07a27a4331f8d
Lautaro Farias <farias.lautaro002@gmail.com>
2024-05-27 18:03:12 -0300

    feat: Implement login in AuthRepository

--- src/domain/repositories/auth.ts
+++ src/domain/repositories/auth.ts
@@ -1,6 +1,7 @@
-import type { RegisterUserDTO } from "../dtos/auth";
-import type { User } from "../entities/user";
+import type { LoginUserDTO, RegisterUserDTO } from "../dtos/auth"
+import type { User } from "../entities/user"
 
 export abstract class AuthRepository {
   abstract register(dto: RegisterUserDTO): Promise<User>
+  abstract login(dto: LoginUserDTO): Promise<User>
 }
--- src/infrastructure/repositories/auth.ts
+++ src/infrastructure/repositories/auth.ts
@@ -1,12 +1,14 @@
-import { AuthDataSource } from "../../domain/datasources/auth";
-import type { RegisterUserDTO } from "../../domain/dtos/auth";
-import type { User } from "../../domain/entities/user";
-import { AuthRepository as _AuthRepository } from "../../domain/repositories/auth";
+import { AuthDataSource } from "../../domain/datasources/auth"
+import type { LoginUserDTO, RegisterUserDTO } from "../../domain/dtos/auth"
+import type { User } from "../../domain/entities/user"
+import { AuthRepository as _AuthRepository } from "../../domain/repositories/auth"
 
 export class AuthRepository implements _AuthRepository {
-  constructor(
-    private readonly datasource: AuthDataSource
-  ) { }
+  constructor(private readonly datasource: AuthDataSource) {}
+
+  login(dto: LoginUserDTO): Promise<User> {
+    return this.datasource.login(dto)
+  }
 
   register(dto: RegisterUserDTO): Promise<User> {
     return this.datasource.register(dto)
