259cd6f9d6f6d75685d57cd68f59e1fb59af4d28
Lautaro Farias <farias.lautaro002@gmail.com>
2024-05-27 17:53:02 -0300

    feat: Implemented login in datasource

--- src/domain/datasources/auth.ts
+++ src/domain/datasources/auth.ts
@@ -1,6 +1,7 @@
-import type { RegisterUserDTO } from "../dtos/auth";
-import type { User } from "../entities/user";
+import type { RegisterUserDTO, LoginUserDTO } from "../dtos/auth"
+import type { User } from "../entities/user"
 
 export abstract class AuthDataSource {
   abstract register(dto: RegisterUserDTO): Promise<User>
+  abstract login(dto: LoginUserDTO): Promise<User>
 }
--- src/infrastructure/datasources/auth.ts
+++ src/infrastructure/datasources/auth.ts
@@ -1,6 +1,6 @@
 import { UserModel } from "../../database/mongodb/models"
 import { AuthDataSource as _AuthDataSource } from "../../domain/datasources/auth"
-import type { RegisterUserDTO } from "../../domain/dtos/auth"
+import type { LoginUserDTO, RegisterUserDTO } from "../../domain/dtos/auth"
 import { User } from "../../domain/entities/user"
 import { HttpError } from "../../domain/errors/http"
 import { UserMapper } from "../mappers/user"
@@ -13,6 +13,25 @@ export class AuthDataSource implements _AuthDataSource {
     private readonly hashPassword: HashFunction,
     private readonly comparePassword: CompareFunction,
   ) {}
+
+  async login(dto: LoginUserDTO) {
+    const { email, password } = dto
+
+    try {
+      const user = await UserModel.findOne({ email })
+
+      if (!user || !this.comparePassword(password, user.password))
+        throw HttpError.badRequest("Invalid credentials")
+
+      return UserMapper.userEntityFromObject(user)
+    } catch (error) {
+      if (error instanceof HttpError) throw error
+
+      console.error(error)
+
+      throw HttpError.internalServerError()
+    }
+  }
 
   async register(dto: RegisterUserDTO): Promise<User> {
     const { name, email, password } = dto
