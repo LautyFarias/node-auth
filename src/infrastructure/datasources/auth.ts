e6e8e7eb959b9f6e328051f7478b1b27107449c2
Lautaro Farias <farias.lautaro002@gmail.com>
2024-05-27 11:21:41 -0300

    style: Format AuthDataSource Impl

--- src/infrastructure/datasources/auth.ts
+++ src/infrastructure/datasources/auth.ts
@@ -1,44 +1,40 @@
-import { UserModel } from "../../database/mongodb/models";
-import { AuthDataSource as _AuthDataSource } from "../../domain/datasources/auth";
-import type { RegisterUserDTO } from "../../domain/dtos/auth";
-import { User } from "../../domain/entities/user";
-import { HttpError } from "../../domain/errors/http";
-import { UserMapper } from "../mappers/user";
-
+import { UserModel } from "../../database/mongodb/models"
+import { AuthDataSource as _AuthDataSource } from "../../domain/datasources/auth"
+import type { RegisterUserDTO } from "../../domain/dtos/auth"
+import { User } from "../../domain/entities/user"
+import { HttpError } from "../../domain/errors/http"
+import { UserMapper } from "../mappers/user"
 
 type HashFunction = (password: string) => Promise<string>
 type CompareFunction = (password: string, hash: string) => Promise<boolean>
 
-
 export class AuthDataSource implements _AuthDataSource {
-
   constructor(
     private readonly hashPassword: HashFunction,
-    private readonly comparePassword: CompareFunction
-  ) { }
+    private readonly comparePassword: CompareFunction,
+  ) {}
 
   async register(dto: RegisterUserDTO): Promise<User> {
-
     const { name, email, password } = dto
 
     try {
-
       const exists = await UserModel.exists({ email })
 
       if (exists) throw HttpError.badRequest("User already exists")
 
-      const user = await UserModel.create({ name, email, password: await this.hashPassword(password) })
+      const user = await UserModel.create({
+        name,
+        email,
+        password: await this.hashPassword(password),
+      })
 
       await user.save()
 
       return UserMapper.userEntityFromObject(user)
-
     } catch (error) {
       if (error instanceof HttpError) throw error
 
       throw HttpError.internalServerError()
     }
-
   }
-
 }
